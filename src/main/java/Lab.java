9951c42930d35a681e81197e851de9c5b162b946
phorjax <joseph.12082@gmail.com>
2023-06-29 20:34:57 +0000

    done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,8 @@
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
+
+
 
 /**
  * Queues are FIFO - first in, first out, like a checkout line. They are used in algorithms where it is needed to keep
@@ -36,7 +39,8 @@ public class Lab {
      * Stack behavior.
      */
     public Deque<String> createQueue(){
-        return null;
+        Queue <String> queue = new LinkedList<>();
+        return (Deque<String>) queue;
     }
 
     /**
@@ -45,7 +49,8 @@ public class Lab {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+        
+        return queue.size();
     }
 
     /**
@@ -55,7 +60,7 @@ public class Lab {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+        queue.add(item);
     }
 
     /**
@@ -65,7 +70,8 @@ public class Lab {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        
+        return queue.remove();
     }
 
     /**
@@ -75,6 +81,7 @@ public class Lab {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+
+        return queue.element();
     }
 }
